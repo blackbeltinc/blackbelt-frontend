5e44de8ca2d0e764b6aadb26964e1b59d38bd52f
Natália Fonseca <nataliabf@outlook.com>
2021-08-27 23:34:52 -0300

    feat: add header with dark mode toggle (#2)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import { Header } from 'components/Header';
+
 export default function Home() {
-  return <h1>BlackBelt</h1>;
+  return <Header />;
 }
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,5 +1,14 @@
 import { extendTheme } from '@chakra-ui/react';
 
-const theme = extendTheme();
+const theme = extendTheme({
+  config: {
+    initialColorMode: 'light',
+    useSystemColorMode: true,
+  },
+  fonts: {
+    heading: 'Lato',
+    body: 'Lato',
+  },
+});
 
 export default theme;
