54b1f0e7182d03b2c703a72f4ad5a80d1b8b511f
Natália Fonseca <nataliabf@outlook.com>
2021-08-27 23:17:37 -0300

    feat: add header with dark mode toggle

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,12 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <title>Black Belt</title>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
