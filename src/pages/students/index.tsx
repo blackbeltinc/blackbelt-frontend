ce0415427bf71001ee237d379c26baf7e1dafd94
Natália Fonseca <nataliabf@outlook.com>
2021-10-19 15:50:12 -0300

    feat: add student list

--- src/pages/students/index.tsx
+++ src/pages/students/index.tsx
@@ -1,14 +1,43 @@
-import { Button, Flex, Heading, Icon } from '@chakra-ui/react';
+/* eslint-disable no-nested-ternary */
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Icon,
+  Spinner,
+  Table,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tr,
+  useBreakpointValue,
+} from '@chakra-ui/react';
 import Link from 'next/link';
-import { RiAddLine } from 'react-icons/ri';
+import { RiAddLine, RiPencilLine } from 'react-icons/ri';
 import { MainContainer } from '../../components/MainContainer';
+import { useStudents } from '../../hooks/useStudents';
 import { withSSRAuth } from '../../utils/withSSRAuth';
 
 export default function StudentList() {
+  const { data, isLoading, isFetching, error } = useStudents();
+
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  });
+
   return (
     <MainContainer>
       <Flex mb="8" justify="space-between" w="100%">
-        <Heading size="lg">Alunos</Heading>
+        <Heading size="lg">
+          Alunos
+          {!isLoading && isFetching && (
+            <Spinner size="sm" color="gray.500" ml="4" />
+          )}
+        </Heading>
         <Link href="/students/create" passHref>
           <Button
             as="a"
@@ -21,6 +50,55 @@ export default function StudentList() {
           </Button>
         </Link>
       </Flex>
+      {isLoading ? (
+        <Flex justify="center">
+          <Spinner />
+        </Flex>
+      ) : error ? (
+        <Flex justify="center">Falha ao obter dados dos usuários.</Flex>
+      ) : (
+        <>
+          <Table colorScheme="whiteAlpha">
+            <Thead>
+              <Tr>
+                <Th>Usuário</Th>
+                {isWideVersion && <Th>Data de Nascimento</Th>}
+                {isWideVersion && <Th width="8" />}
+              </Tr>
+            </Thead>
+            <Tbody>
+              {data.map((student) => (
+                <Tr key={student.id}>
+                  <Td>
+                    <Box>
+                      <Text fontWeight="bold">
+                        {student.first_name} {student.last_name}
+                      </Text>
+                      <Text fontSize="sm" color="gray.300">
+                        {student.email}
+                      </Text>
+                    </Box>
+                  </Td>
+                  {isWideVersion && <Td>{student.birthdate}</Td>}
+                  {isWideVersion && (
+                    <Td>
+                      <Button
+                        as="a"
+                        size="sm"
+                        fontSize="sm"
+                        colorScheme="blackbelt"
+                        leftIcon={<Icon as={RiPencilLine} />}
+                      >
+                        Editar
+                      </Button>
+                    </Td>
+                  )}
+                </Tr>
+              ))}
+            </Tbody>
+          </Table>
+        </>
+      )}
     </MainContainer>
   );
 }
