a82e2f0a98714c0fa75df28ceecc964dd1d5d7c6
Lucas Souza <lucassouza.1000@hotmail.com>
2022-04-06 21:16:15 -0300

    teste nav bar

--- src/components/Sidebar/SidebarNav.tsx
+++ src/components/Sidebar/SidebarNav.tsx
@@ -26,6 +26,11 @@ export function SidebarNav() {
             Aulas
           </NavLink>
         </NavSection>
+        <NavSection title="CAMPEONATOS">
+          <NavLink href="/classes" icon={BiChalkboard}>
+            Campeonatos
+          </NavLink>
+        </NavSection>
         <NavSection title="PERFIL">
           <Button variant="link" onClick={signOut}>
             <Icon as={RiLogoutCircleLine} fontSize="20" />
